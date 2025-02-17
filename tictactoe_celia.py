74b4a0d25e69df55f90e6cd6db45cc0c4c255401
celichu <celchese@gmail.com>
2025-02-16 21:02:53 -0800

    Update tictactoe_celia.py - working version
    working version of tictactoe

--- tictactoe_celia.py
+++ tictactoe_celia.py
@@ -21,7 +21,18 @@ import sys # used to exit when quit is given as input
 
 # Board Dictionary
 TTTBoard = {"A": "A", "B": "B", "C": "C", "D": "D", "E": "E", "F": "F", "G": "G", "H": "H", "I": "I"}
+
+#fix print opening credits centered.
 Centerwidth = 60 
+OpCreditText= """
+### TEAM SEA ###
+#     (..)     #
+#   (((())))   #
+####PRESENTS####
+##TIC#TAC#TOE###
+"""
+
+###### WORKING LINE
 # salutation
 #needs import random
 def salutation():
@@ -31,15 +42,15 @@ def salutation():
 
 #Utility function
 # exits if 'quit' is entered
-# saves input to file
-logfile = 'tttlog.txt'
+# saves input passed through it to file
+savefile = 'tictactoe.txt'
 def utility(Input):
         if Input.upper() == 'QUIT': # quit if quit is entered
-            with open(logfile, 'a') as save_actions: # a = append ##next line = pretty as new line in file
+            with open(savefile, 'a') as save_actions: # a = append ##next line = pretty as new line in file
                 save_actions.write(f'User input quit request: {Input}\n')  #\n for new line
             sys.exit()
         # logging # optional
-        with open(logfile, 'a') as save_actions: # a = append ##next line = pretty as new line in file
+        with open(savefile, 'a') as save_actions: # a = append ##next line = pretty as new line in file
             save_actions.write(f'User Input: {Input}\n') # Quotes don't work, must be ' #\n for new line
 
 
@@ -55,7 +66,6 @@ def get_player_name(NameAsk="Enter your name:"):
         return PlayerName
       except ValueError:
             print(f"Please try again-{PlayerName} is not valid. Provide a name, or 'quit' to exit")
-
 
 
 # select marker function / player
@@ -75,6 +85,7 @@ def select_marker(Player):
          print("Please enter x, o, or quit to exit")
 
 #versus function, Designates marker of opponent based on
+# player 1s input
 def select_versus(Player, PlayerMarker):
 #   if Player1Marker == "X"
     print(f"versus function:{Player} is playing {PlayerMarker}")
@@ -98,81 +109,106 @@ def TTTBoard_Display():
     print(f"| {TTTBoard['G']} | {TTTBoard['H']} | {TTTBoard['I']} |") 
     print("+-----------+")
 
-# win conditions function # fix
-def CheckForWin(Player):
+## win conditions function ###
+# returns false until a given marker is present in all three slots of one of the conditions
+def CheckForWin(PlayerMarker):
     WinConditions = [
         ["A","B","C"],["D","E","F"],["G","H","I"],# Horizontal
         ["A","D","G"],["B","E","H"],["C","F","I"],# Vertical
         ["A","E","I"],["G","E","C"] # Diagonal
     ]
     for Conditions in WinConditions:
-        if TTTBoard[Conditions[0]] == TTTBoard[Conditions[1]] == TTTBoard[Conditions[2]] == Player:
+        if TTTBoard[Conditions[0]] == TTTBoard[Conditions[1]] == TTTBoard[Conditions[2]] == PlayerMarker:
             return True
     return False
 
-## Move Logic
-#player move
+### Move Logic
+# player move function
 def TTT_Move(CurrentPlayer,PlayerMarker):
     while True:
         PlaceMark = input(f"Player {CurrentPlayer}, enter A-I to place your {PlayerMarker} ").upper()
-        utility(PlaceMark)
+        utility(PlaceMark) # pass input through the utility function that logs the input and checks for 'quit' 
+        # if input is in the list of moves
         if PlaceMark in TTTBoard:
-            TTTBoard[PlaceMark] = PlayerMarker
-            TTTBoard_Display()
-            break
+            print(TTTBoard[PlaceMark])
+            # check if the location is already taken.
+            if TTTBoard[PlaceMark] not in ["X","O"]:
+                TTTBoard[PlaceMark] = PlayerMarker
+                TTTBoard_Display()
+                break
+            else:
+                print(f"Oops! {PlaceMark} is taken by an {TTTBoard[PlaceMark]}. try again!")
         else:
             print(f"Select a letter A-I to place your {PlayerMarker}, or quit to exit.")
 
-#function to have computer randomly select move
+#function to have computer randomly select move ###
 def Octo_Move(CurrentPlayer,PlayerMarker):
         PlacePick = random.randint(0, len(TTTBoard) - 1)
+        # using list of places as dictionary used for the board is not positional.
         Places = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
-        #Places = [TTTBoard]
         print(f"{CurrentPlayer} is playing {Places[PlacePick]}")
-        if Places[PlacePick] in TTTBoard:
+        #check if location is taken
+        if TTTBoard[Places[PlacePick]] not in ["X","O"]:
             TTTBoard[Places[PlacePick]] = PlayerMarker
             TTTBoard_Display()
+        else:
+            print(f"{Places[PlacePick]} is taken, trying again.")
 
 
-
-# versus_PCorNPC
-#def versus_PCorNPC()
-#print(f"Will {OtherMarker} be played by another player or the computer")
-#SelectPlayer = input("1 for a player, 2 to play the computer:")
-
-
-# turn logic
-# def first_move(Player1Marker):
-#     if Player1Marker == "X":
-#         TTT_Move(Player1,Player1Marker)
-#     else:
-#         Octo_Move(Player2,Player2Marker)
+#Function to switch between players
+#using index properties of lists
+def switch_player(CurrentPlayer, Players):
+    #print("switching...", CurrentPlayer)
+    current_index = Players.index(CurrentPlayer)
+    #print("current_index.", current_index)
+    next_index = (current_index + 1) % len(Players)
+    #print("next_index.", next_index)
+    #print("switching to:", Players[next_index])
+    return Players[next_index]
 
 
-
+# logic for first move, so that x always goes first
 def first_move():
+    global Player1Marker
     if Player1Marker == "X":
         TTT_Move(Player1,Player1Marker)
-        CurrentPlayer = Player1#,Player1Marker
-        NextPlayer = Player2#,Player2Marker
-        return CurrentPlayer,NextPlayer
+        CurrentPlayer = [Player1,Player1Marker]
+        print("first move completed, current player:", CurrentPlayer)
+        CurrentPlayer = switch_player(CurrentPlayer,PlayerList)
+        return CurrentPlayer
     else:
         Octo_Move(Player2,Player2Marker)
-        CurrentPlayer = Player2#,Player2Marker
-        NextPlayer == Player1#,Player1Marker
-        return CurrentPlayer,NextPlayer
-
-def play_TTT():
-    #while CheckForWin() is False:
-    print(CheckForWin)
-    for player in Players:
-       if player == Player2:
-        Octo_Move(Player2,Player2Marker)
-    else:
-        TTT_Move(Player1,Player1Marker)
+        CurrentPlayer = [Player2,Player2Marker]
+        print("first move completed, current player:", CurrentPlayer)
+        CurrentPlayer = switch_player(CurrentPlayer,PlayerList)
+        return CurrentPlayer
     
+      
 
 ### MAIN LOGIC ## 
+# Main TicTacToe Loop
+def TTT_Main():
+    global CurrentPlayer
+    while CheckForWin(CurrentPlayer[1]) is False:
+        if CurrentPlayer[0] == "Placeholder PC":
+            Octo_Move(CurrentPlayer[0],CurrentPlayer[1])
+        else:
+            TTT_Move(CurrentPlayer[0],CurrentPlayer[1])
+        if CheckForWin(CurrentPlayer[1]) is True:
+            print(f"Player {CurrentPlayer[0]} won!")
+            sys.exit()
+        else:
+            pass
+        CurrentPlayer = switch_player(CurrentPlayer,PlayerList)
+        print("switching to: ",CurrentPlayer[0])
+        TTT_Main()
+    else:
+        print(f"Player {CurrentPlayer[0]} won!")
+        sys.exit()
+
+# while no one has one
+#    while CheckForWin(TestPlayerMarker) is False:
+
 #call greeting
 salutation()
 #get player 1 name
@@ -183,21 +219,20 @@ print(f"{Player1} is playing {Player1Marker}")
 #Set Player 2 to computer
 Player2Marker= select_versus(Player1,Player1Marker)
 Player2 = "Placeholder PC" #placeholder?
-Players = [Player1,Player2]
-
 print(f"{Player2} is playing {Player2Marker}")
+
+#Define player list used for switching
+PlayerList = [[Player1, Player1Marker], [Player2 , Player2Marker]]
+
+# display the board
 TTTBoard_Display()
-  #TTT_Move()
-    #first_move()
 
 
-first_move()
-#print(f"Current Player is {CurrentPlayer}")
-play_TTT()
+#status print - may be decommented to observe logic progress
+print(f"Players gathered, starting first move")
 
 
-
-    #CurrentPlayer=first_move()
-# call main
-#def main():
-#main()
+CurrentPlayer= first_move()
+#status print - may be decommented to observe logic progress
+print(f"First Move complete: Current Player is {CurrentPlayer[0]}")
+TTT_Main()
